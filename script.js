98296917417368ea27a39f2c705c3a9a11144a09
Israel Adefidipe <israeladefidipe@gmail.com>
2021-02-11 22:54:15 +0100

    Functions done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+'use strict'; // to activate strict mode in js
 // const weightMark = 78;
 // const heightMark = 1.69;
 // const weightJohn = 92;
@@ -84,29 +84,97 @@
 //         console.log(`your tip is 20%`)
 
 // }
-// switch statement
-const day = 'monday';
-switch (day){
-    case 'monday':
-        console.log("It is monday");
-        break;
-    case'tuesday':
-    case 'wednesday':
-        console.log("the two days");
-        break;
-    default:
-        console.log("Not a week day")
-}
-// if statement equivalent
+// // switch statement
+// const day = 'monday';
+// switch (day){
+//     case 'monday':
+//         console.log("It is monday");
+//         break;
+//     case'tuesday':
+//     case 'wednesday':
+//         console.log("the two days");
+//         break;
+//     default:
+//         console.log("Not a week day")
+// }
+// // if statement equivalent
 
-if (day === 'monday'){
-    console.log('It is monday')
-    }else if (day === 'tuesday' || day === 'wednesday'){
-    console.log('the two days')    
-    }else {
-        console.log('Not a week day')
-    }
-// using conditinal operators
-const bill = 275;
-const tip = bill>=50 && bill<=300 ? bill * (15/100): bill = 20/100;
-console.log(`The bill is ${bill}, tip is ${tip}, total value is ${bill + tip}`)
+// if (day === 'monday'){
+//     console.log('It is monday')
+//     }else if (day === 'tuesday' || day === 'wednesday'){
+//     console.log('the two days')    
+//     }else {
+//         console.log('Not a week day')
+//     }
+// // using conditinal operators
+// const bill = 275;
+// const tip = bill>=50 && bill<=300 ? bill * (15/100): bill = 20/100;
+// console.log(`The bill is ${bill}, tip is ${tip}, total value is ${bill + tip}`)
+
+
+// // Funtions
+// function logger() {
+//     console.log( 'My name is Israel'); //function body
+// }
+
+// console.log (typeof (logger())) //invoking or calling the function
+
+// function fruitProcessor (apples,oranges){
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice; //this wil always be the result whe this funtion is called
+// }
+
+// const appleJuice = fruitProcessor(6,7);
+// console.log(appleJuice)
+
+// function calAge1(birthYear) {
+//     return 2020 - birthYear;
+// }
+// console.log ( calAge1(1996));
+
+// // Function expression
+// const age2 =function (birthYear){
+//     return 2020 - birthYear;
+// }
+// console.log (age2(1997));
+
+// // Arrow funtion
+// const age3 = birthYear => 2020 - birthYear;
+// console.log(age3(2000))
+
+// const yearsUntilRetirement = birthYear =>{
+//     const age = 2020 - birthYear;
+//     const retirement = 65 - age;
+//     return retirement;
+// }
+
+// console.log ( yearsUntilRetirement(1996));
+
+// arrow functionsctions with two variables
+// const yearsUntilRetirement = (birthYear, firstName) =>{
+//     const age = 2020 - birthYear;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return `${firstName} retires in ${retirement} years`; 
+// }
+
+// console.log(yearsUntilRetirement(1995, 'Folarin'));
+
+// funtions calling other funtions
+const cutPieces = function (fruit) {
+    return fruit * 4;
+}
+
+const fruitProcessor = function (apples, oranges){
+    const applesPieces = cutPieces(apples);
+    const orangesPieces = cutPieces(oranges);
+
+    const juice = `Juice with ${applesPieces} apples and ${orangesPieces} oranges`;
+    return juice;
+
+}
+
+console.log( fruitProcessor(3,7))
+
+
